f823c2840fe1ce29d37ea93bfcd61edb55579d62
tyraelHqy <752546894@qq.com>
2020-05-16 21:19:39 +0800

    1. Spring项目初始化

---
+++ src/test/java/com/tyrael/tools/ToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tyrael.tools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
